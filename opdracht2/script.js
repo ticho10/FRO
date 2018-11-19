706580e8cbf775ee5fe96b85fcd9eb0a086ed747
ticho10 <poffertjeskip@gmail.com>
2018-11-19 16:33:06 +0100

    commit

---
+++ opdracht2/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    //test
+    // $('main').hide(3000).delay(1000).show(500);
+    $('.portret img').mouseenter(function () {
+        $(this).fadeTo(1000, .1);
+    }).mouseleave( function () {
+        $(this).fadeTo(2000,1);
+    }).click(function () {
+        $(this).next().toggle(2000);
+    });
+
+//    footer
+    $('footer').click(function () {
+        $(this).animate({
+            top:'-=100px',
+            opacity: '-=0.2'
+        },1000).dblclick(function () {
+            $(this).animate({
+                top : '0',
+                opacity: '1'
+            },3000)
+        })
+    })
+
+}); //einde document ready()
